60d08e96afc071a2672b1959b61d46198a585292
su-mahesh <maheshkangude@gmail.com>
2021-01-20 17:36:30 +0530

    ADDED: mobile number validation

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -5,6 +5,7 @@ public class UserRegistration {
     final private String firstNamePattern = "[A-Z][a-z]{2,}";
     final private String lastNamePattern = "[A-Z][a-z]{2,}";
     final private String emailAddressPattern = "[A-Za-z0-9_][+A-Za-z0-9_-]*[.]?[+A-Za-z0-9_-]+[@]([A-Za-z0-9]+[.][A-Za-z]{2,63}|[A-Za-z0-9-]{2,}[.][A-Za-z0-9]{2,63}[.][A-Za-z]{2,3})";
+    final private String mobileNumberPattern = "^[1-9][0-9]{0,2}\\s[0-9]{10}";
 
     public boolean firstNameTesting(String firstName) {
         return Pattern.matches(firstNamePattern, firstName);
@@ -18,12 +19,20 @@ public class UserRegistration {
         return Pattern.matches(emailAddressPattern, emailAddress);
     }
 
+    public boolean mobileNumberTesting(String mobileNumber) {
+        return Pattern.matches(mobileNumberPattern, mobileNumber);
+    }
+
     public static void main(String[] args) {
         UserRegistration userRegistration = new UserRegistration();
 
         System.out.println(userRegistration.firstNameTesting("Mahesh"));
         System.out.println(userRegistration.lastNameTesting("Kangude"));
         System.out.println(userRegistration.emailAddressTesting("abc@com.in"));
+        System.out.println(userRegistration.mobileNumberTesting("91 8473652883"));
+
+
+
     }
 }
 
