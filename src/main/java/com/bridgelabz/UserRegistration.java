76fa77541e2307f1158ab10f94de6cd0526796b4
su-mahesh <maheshkangude@gmail.com>
2021-01-20 17:29:21 +0530

    ADDED: last name validation

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -3,15 +3,21 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     final private String firstNamePattern = "[A-Z][a-z]{2,}";
+    final private String lastNamePattern = "[A-Z][a-z]{2,}";
 
     public boolean firstNameTesting(String firstName) {
         return Pattern.matches(firstNamePattern, firstName);
+    }
+
+    public boolean lastNameTesting(String lastName) {
+        return Pattern.matches(lastNamePattern, lastName);
     }
 
     public static void main(String[] args) {
         UserRegistration userRegistration = new UserRegistration();
 
         System.out.println(userRegistration.firstNameTesting("Mahesh"));
+        System.out.println(userRegistration.firstNameTesting("Kangude"));
     }
 }
 
