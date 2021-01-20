3d66da08be1854e190f618ad65a1778d97409563
su-mahesh <maheshkangude@gmail.com>
2021-01-20 17:46:01 +0530

    ADDED: password validation exactly one special char

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -6,7 +6,7 @@ public class UserRegistration {
     final private String lastNamePattern = "[A-Z][a-z]{2,}";
     final private String emailAddressPattern = "[A-Za-z0-9_][+A-Za-z0-9_-]*[.]?[+A-Za-z0-9_-]+[@]([A-Za-z0-9]+[.][A-Za-z]{2,63}|[A-Za-z0-9-]{2,}[.][A-Za-z0-9]{2,63}[.][A-Za-z]{2,3})";
     final private String mobileNumberPattern = "^[1-9][0-9]{0,2}\\s[0-9]{10}";
-    final private String passwordPattern = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
+    final private String passwordPattern = "^(?=.{8,20}$)(?=.*[0-9])(?=.*[A-Z])[A-Za-z0-9]*[^A-Za-z0-9][A-Za-z0-9]*$";
     public boolean firstNameTesting(String firstName) {
         return Pattern.matches(firstNamePattern, firstName);
     }
@@ -35,7 +35,7 @@ public class UserRegistration {
         System.out.println(userRegistration.lastNameTesting("Kangude"));
         System.out.println(userRegistration.emailAddressTesting("abc@com.in"));
         System.out.println(userRegistration.mobileNumberTesting("91 8473652883"));
-        System.out.println(userRegistration.passwordesting("fefeJrlm9fefef"));
+        System.out.println(userRegistration.passwordesting("fefe&Jrlm9fefef"));
 
     }
 }
